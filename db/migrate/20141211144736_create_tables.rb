59b693e0d2b02e69779084325b13dd4236c49f77
cmliotta & Vincent Colavin <pair+cmliotta.vcolavin@devbootcamp.com>
2014-12-11 14:57:32 -0800

    add models and migrations

---
+++ db/migrate/20141211144736_create_tables.rb
@@ -0,0 +1,13 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_hash
+      t.integer :hoo_id # giftee
+      t.integer :group_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20141211145145_create_group_table.rb
@@ -0,0 +1,9 @@
+class CreateGroupTable < ActiveRecord::Migration
+  def change
+    create_table :groups do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
