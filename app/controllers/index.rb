420604322639d9006d661d6473b5ce8e41d468fe
cmliotta & Vincent Colavin <pair+cmliotta.vcolavin@devbootcamp.com>
2014-12-11 18:37:21 -0800

    some stuff idk

---
+++ app/controllers/index.rb
@@ -0,0 +1,46 @@
+get '/' do
+  # render home page
+ #TODO: Show all users if user is signed in
+  erb :index
+end
+
+#----------- SESSIONS -----------
+
+# login and create session
+get '/login' do
+  erb :login
+end
+post '/login' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/group/#{user.id}"
+  else
+    redirect '/'
+  end
+end
+
+# log out
+delete '/group/:user_id' do
+  session.delete(:user_id)
+  redirect '/'
+end
+
+get '/group/:user_id' do
+  if session[:user_id]
+    @user_id = params[:user_id]
+    erb :group
+  else
+    redirect '/'
+  end
+end
+
+#----------- USERS -----------
+
+get '/users/new' do
+  erb :sign_up
+end
+post '/users' do
+  User.create!(params[:user])
+  redirect to '/login'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  def authenticate(password)
+    self.password_hash = password
+  end
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
 end
